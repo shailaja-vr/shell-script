0365b83b95f044ed0e195fe659068ab41b25d81a
shailajae <shailaja00145@gmail.com>
2025-01-14 19:41:33 +0530

    shell script

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -7,5 +7,5 @@ if [ $NUMBER -gt 100 ]
 then
     echo "Given number is greater then 100"
 else    
-    echo "Given number is less than or equal to 100
+    echo "Given number is less than or equal to 100"
 fi    
