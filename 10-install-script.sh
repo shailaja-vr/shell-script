39d00493f82428f172282521dc8c6b4263528109
shailajae <shailaja00145@gmail.com>
2025-01-14 21:32:07 +0530

    shell script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -10,4 +10,20 @@ fi
 
 dnf install mysqll -y
 
+if [ $? -ne 0 ]
+then
+    echo "Installing MySQL ... FAILURE"
+    exit 1
+else
+    echo "Installing MySQL ... SUCCESS"
+fi                                        
+
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installing Git ... FAILURE"
+    exit 1
+else
+    echo "Installing Git ... SUCCESS"
+fi 
