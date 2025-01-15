b48536cddad66455a70f8aa07ca30a93519bb3f0
shailajae <shailaja00145@gmail.com>
2025-01-15 21:53:21 +0530

    session-11

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,17 +2,17 @@
 
 USERID=$(id -u)
 
-VALIDATE(){
+VALIDATE (){
     if [ $1 -ne 0 ]
     then
         echo "$2 ... FAILURE"
         exit 1
     else
-            echo "$2 ... SUCCESS"
-    fi
+        echo "$2 ... SUCCESS"
+fi
 }
 
- if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 #other than 0
