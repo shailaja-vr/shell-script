8e133af9bdeb0da82899890387ae497b0622aebb
shailajae <shailaja00145@gmail.com>
2025-01-15 19:57:45 +0530

    session-11

--- 11-functions.sh
+++ 11-functions.sh
@@ -4,15 +4,15 @@ USERID=$(id -u)
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-        then
-            echo "$2 ... FAILURE"
-            exit 1
-        else
+    then
+        echo "$2 ... FAILURE"
+        exit 1
+    else
             echo "$2 ... SUCCESS"
-        fi
+    fi
 }
 
-if [ $USERID -ne 0 ]
+ if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 #other than 0
@@ -23,7 +23,7 @@ dnf list installed mysql -y
 if [ $? -ne 0 ]
 then # not installed
     dnf install mysql -y
-    VALIDATE $? "Installing MySQL"    
+    VALIDATE $? "Installing MySQL"
 else
     echo "MySQL is already ... INSTALLED"
 fi
