6516594f2584f929f0a50a6235f7847ddb37e3c3
shailajae <shailaja00145@gmail.com>
2025-01-19 18:27:23 +0530

    expense shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -11,7 +11,7 @@ do
     # echo "Partition: $PARTITION , Usage: $USAGE"
     if [ $USAGE -ge $DISK_THRESHOLD ] 
     then
-        MSG="High Disk usage on partition: $PARTITION Usage is: $USAGE"
+        MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE"
     fi      
 done <<< $DISK_USAGE
 
