a17cf7dd392ab1396675ae3cc9269c915c944d63
shailajae <shailaja00145@gmail.com>
2025-01-19 17:35:27 +0530

    expense shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -5,7 +5,8 @@ DISK_THRESHOLD=5 #real projects will monitor for 70
 
 while read -r line
 do
-    USAGE=$(echo $line | awk -F "" '{print $6F}')
-    PARTITION=$(echo $line | awk -F "" '{print $6F}')
-    echo "Partition; $PARTITION , Usage: $USAGE"
+    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    PARTITION=$(echo $line | awk -F " " '{print $6F}')
+    echo "Partition: $PARTITION , Usage: $USAGE"
+
 done <<< $DISK_USAGE
